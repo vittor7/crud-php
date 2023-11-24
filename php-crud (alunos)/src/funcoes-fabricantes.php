e313a54ffad1f0641fbf34726e1eec76cbf0f464
vitorGEMIDA <hugov3141v@gmail.com>
2023-11-24 08:30:56 -0300

    1

---
+++ php-crud (alunos)/src/funcoes-fabricantes.php
@@ -0,0 +1,16 @@
+<?php
+    // Incluir neste ponto o arquivo conecta.php 
+    
+    // Programar a função lerFabricantes neste ponto
+    
+    // Inserir um fabricante (PDO - PHP Database Object)
+    // Obs void indica que a função não tem retorno "return"
+
+    // Programar a função inserirFabricante neste ponto
+    
+    // Programar a função lerUmFabricante neste ponto
+    
+    // Programar a função atualizarFabricante neste ponto
+    
+    // Programar a função excluirFabricante neste ponto
+    
